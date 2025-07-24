46e0b55b9659e16911acc11cc3b955bf649593da
Aaron Cuellar <aaroncuellar2003@gmail.com>
2025-07-23 21:34:32 -0300

    resolved guia00

--- Guias/Soluciones/guia00.hs
+++ Guias/Soluciones/guia00.hs
@@ -1,148 +1,186 @@
----------- GUIA 0 - REPASO ----------
-
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use notElem" #-}
+{-# HLINT ignore "Use foldl" #-}
+{-# import qualified Main as 2 #-}
 {-# HLINT ignore "Evaluate" #-}
 {-# HLINT ignore "Use infix" #-}
 {-# HLINT ignore "Use id" #-}
 {-# HLINT ignore "Use record patterns" #-}
 
+---------- GUIA 0 - REPASO HASKELL ----------
 
 ----- EJERCICIO 1
--- usar: null head tail init last take drop (++) concat reverse elem, provenientes del prelude
 
-isNull1 = null [2]
-isNull2 = null [2, 3, 5]
+{-
+null toma una lista y devuelve True si la lista está vacía, False en caso contrario.
 
-lista = [1, 2, 3, 4, 5]
-listOfList = [[1], [2, 3, 4]]
+null :: [a] -> Bool
+-}
 
-cabeza = head lista
-cola = tail lista
-ultimo = last lista
-primeros3 = take 3 lista
-deleteFirst2 = drop 2 lista
-deleteLast = init lista
-concatenacion = [1,2,3] ++ [4,5,6]
-concatListOfList = concat listOfList
-listaInvertida = reverse lista
-pertenece6 = elem 6 lista
-pertenece2 = 2 `elem` lista
+{-
+head toma una lista y devuelve el primer elemento de la misma, si la lista está vacía devuelve error
 
------- EJERCICIO 2
--- definir las siguientes funciones:
--- a. valorAbsoluto :: Float → Float, que dado un número devuelve su valor absoluto.
--- b. bisiesto :: Int → Bool, que dado un número que representa un año, indica si el mismo es bisiesto.
--- c. factorial :: Int → Int, definida únicamente para enteros positivos, que computa el factorial.
--- d. cantDivisoresPrimos :: Int → Int, que dado un entero positivo devuelve la cantidad de divisores primos.
+head :: [a] -> a
+-}
 
+{-
+tail toma una lista y devuelve la lista sin el primer elemento, si la lista está vacía devuelve error
+
+tail :: [a] -> [a]
+-}
+
+{-
+init toma una lista y devuelve la lista sin el ultimo elemento, si la lista está vacía devuelve error
+
+init :: [a] -> [a]
+-}
+
+{-
+last toma una lista y devuelve el ultimo elemento, si la lista está vacía devuelve error
+
+last :: [a] -> a
+-}
+
+{-
+take toma un numero n y una lista, devuelve una lista con los primeros n elementos
+
+take :: Int -> [a] -> [a]
+-}
+
+{-
+drop toma un numero n y una lista, devuelve la lista sin los primeros n elementos
+
+drop :: Int -> [a] -> [a]
+-}
+
+{-
+(++) toma dos listas, concatena las listas en una sola.
+
+(++) :: [a] -> [a] -> [a]
+-}
+
+{-
+concat toma una lista de listas, devuelve la concatenacion de las sublistas en una lista
+
+concat :: [[a]] -> [a]
+-}
+
+{-
+reverse toma una lista, devuelve la lista con orden inverso
+
+reverse :: [a] -> [a]
+-}
+
+{-
+elem toma un elem e y una lista, devuelve True si e pertenece a la lista
+
+elem :: a -> [a] -> Bool
+-}
+
+----- EJERCICIO 2
+
+-- 2.a
 valorAbsoluto :: Float -> Float
-valorAbsoluto x = if x > 0 then x else -x
+valorAbsoluto x
+  | x < 0 = -x
+  | otherwise = x
 
-
+-- 2.b
 esDivisible :: Int -> Int -> Bool
 esDivisible x y = x `mod` y == 0
 
-bisiesto1 :: Int -> Bool
-bisiesto1 year
-          | esDivisible year 400 = True
-          | esDivisible year 4 && not (esDivisible year 100) = True
-          | otherwise = False
+bisiesto :: Int -> Bool
+bisiesto year = esDivisible year 4 && (not (esDivisible year 100) || esDivisible year 400)
 
-bisiesto2 :: Int -> Bool
-bisiesto2 year = esDivisible year 400 || esDivisible year 4 && not (esDivisible year 100)
-
+-- 2.c
 factorial :: Int -> Int
-factorial n | n == 1 = 1
-            | otherwise = n * factorial (n-1)
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
 
+-- 2.d
 esPrimo :: Int -> Bool
-esPrimo n | n <= 1 = False
-          | otherwise = null ([x | x <- [2..(n-1)], esDivisible n x])
+esPrimo n
+  | n < 2 = False
+  | otherwise = null [x | x <- [2 .. (n - 1)], esDivisible n x]
 
 cantDivisoresPrimos :: Int -> Int
-cantDivisoresPrimos n = length [x | x <- [1..n], esDivisible n x && esPrimo x]
+cantDivisoresPrimos n = length [x | x <- [1 .. n], esDivisible n x && esPrimo x]
 
------- EJERCICIO 3
--- definir las siguientes funciones:
--- a. Definir la función inverso :: Float → Maybe Float que dado un número devuelve su inverso multiplicativo si está definido, o Nothing en caso contrario.
--- b. Definir la función aEntero :: Either Int Bool → Int que convierte a entero una expresión que puede ser booleana o entera. En el caso de los booleanos, el entero que corresponde es 0 para False y 1 para True
+-- EJERCICIO 3
 
+{-
+data Maybe a = Nothing | Just a -> el tipo Maybe a (con a un tipo cualquiera) devuelve Nothing (indica ausencia de valor) o Just a (indica presencia de un valor de tipo a)
+
+data Either a b = Left a | Right b -> el tipo Either a b (con a y b dos tipos cualquiera) devuelve Left a (en caso de error) o Right b (en caso de exito)
+-}
+
+-- 3.a
 inverso :: Float -> Maybe Float
-inverso x | x == 0 = Nothing
-          | otherwise = Just (1/x)
+inverso 0 = Nothing
+inverso x = Just (1 / x)
 
-aEntero :: Either Int Bool -> Int
-aEntero = either
-      (\n -> n)
-      (\bool -> if bool then 1 else 0)
+-- 3.b
+aEntero1 :: Either Int Bool -> Int
+aEntero1 (Left n) = n
+aEntero1 (Right bool) = if bool then 1 else 0
 
------- EJERCICIO 4
--- definir las siguientes funciones:
--- a. limpiar :: String → String → String, que elimina todas las apariciones de cualquier caracter de la primera cadena en la segunda. Por ejemplo, limpiar ‘‘susto’’ ‘‘puerta’’ evalúa a ‘‘pera’’. Nota: String es un renombre de [Char]. La notación ‘‘hola’’ es equivalente a [‘h’,‘o’,‘l’,‘a’] y a ‘h’:‘o’:‘l’:‘a’:[].
--- b. difPromedio :: [Float] → [Float] que dada una lista de números devuelve la diferencia de cada uno con el promedio general. Por ejemplo, difPromedio [2, 3, 4] evalúa a [-1, 0, 1].
--- c. todosIguales :: [Int] → Bool que indica si una lista de enteros tiene todos sus elementos iguales.
+aEntero2 :: Either Int Bool -> Int
+aEntero2 =
+  either
+    (\n -> n)
+    (\bool -> if bool then 1 else 0)
 
+-- EJERCICIO 4
+
+-- 4.a
+-- mediante filter recorro s2 y si no esta en s1, lo dejo, caso contrario elimino
 limpiar1 :: String -> String -> String
 limpiar1 [] s2 = s2
-limpiar1 (s1:xs1) s2 = limpiar xs1 [letra | letra <- s2, letra /= s1]
+limpiar1 s1 s2 = filter (\letra -> not (elem letra s1)) s2
 
+-- mediante recursion y lista por comprension, recorro s2 y si no esta en s1 (head de la palabra) la dejo, mismo proceso pero sin el head
 limpiar2 :: String -> String -> String
 limpiar2 [] s2 = s2
-limpiar xs s2 = foldl (\ s2 s1 -> [letra | letra <- s2, letra /= s1]) s2 xs
+limpiar2 (s1 : xs1) s2 = limpiar2 xs1 [letra | letra <- s2, letra /= s1]
 
+-- recorro s1, aplicando la funcion con el acc
+-- la funcion toma el acc y cada elem de s1, devuelve lista donde saco la letra si pertenecia a s1
+limpiar3 :: String -> String -> String
+limpiar3 [] s2 = s2
+limpiar3 s1 s2 = foldl (\s2 xs -> [letra | letra <- s2, letra /= xs]) s2 s1
 
+-- 4.b
 promedio :: [Float] -> Float
-promedio nums = sum nums / fromIntegral (length nums)
+promedio xs = sum xs / fromIntegral (length xs)
 
 difPromedio :: [Float] -> [Float]
-difPromedio nums = [n - promedio nums | n <- nums]
+difPromedio [] = []
+difPromedio nums = map (\num -> num - promedio nums) nums
 
-
+-- 4.c
 todosIguales1 :: [Int] -> Bool
-todosIguales1 [] = True
-todosIguales1 [x] = True
-todosIguales1 (x : xs) = replicate (length xs) x == xs
+todosIguales1 (x : xs) = all (== x) (x : xs)
 
 todosIguales2 :: [Int] -> Bool
-todosIguales2 [] = True
-todosIguales2 [x] = True
-todosIguales2 (x : xs) = all (==x) xs
+todosIguales2 (x : xs) = replicate (length xs) x == xs
 
------- EJERCICIO 5
--- definir las siguientes funciones, teniendo en cuenta el siguiente modelo: data AB a = Nil | Bin (AB a) a (AB a).  
--- a. vacioAB :: AB a → Bool que indica si un árbol es vacío (i.e. no tiene nodos).
--- b. negacionAB :: AB Bool → AB Bool que dado un árbol de booleanos construye otro formado por la negación de cada uno de los nodos.
--- c. productoAB :: AB Int → Int que calcula el producto de todos los nodos del árbol.
-
--- tipo de dato AB
+-- EJERCICIO 5
 data AB a = Nil | Bin (AB a) a (AB a)
 
--- a.
+-- el tipo AB a (con a un tipo cualquiera) devuelve Nil en caso de arbol vacio o los subarboles y el valor del nodo
+
+-- 5.a
 vacioAB :: AB a -> Bool
 vacioAB Nil = True
-vacioAB (Bin _ _ _) = False
+vacioAB _ = False
 
--- arbol vacio
-arbolVacio :: AB Int
-arbolVacio = Nil
-
--- arbol con un nodo
-arbolConNodo :: AB Int
-arbolConNodo = Bin Nil 1 Nil
-
--- b.
+-- 5.b
 negacionAB :: AB Bool -> AB Bool
 negacionAB Nil = Nil
-negacionAB (Bin izq a der) = Bin (negacionAB izq) (not a) (negacionAB der)
+negacionAB (Bin izq v der) = Bin (negacionAB izq) (not v) (negacionAB der)
 
+-- 5.c
 productoAB :: AB Int -> Int
 productoAB Nil = 1
-productoAB (Bin izq a der) = a * productoAB izq * productoAB der
-
-
-
-
-
-
-
-
+productoAB (Bin izq v der) = v * productoAB izq * productoAB der
