207fd701b3baa1cfe7ba009db95ab4a17c6339d9
Aaron Cuellar <aaroncuellar2003@gmail.com>
2025-07-26 22:47:03 -0300

    ex 3a, 3b, 3c

--- Guias/Soluciones/guia01.hs
+++ Guias/Soluciones/guia01.hs
@@ -1,4 +1,8 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Eta reduce" #-}
+{-# HLINT ignore "Use sum" #-}
+{-# HLINT ignore "Avoid lambda" #-}
+{-# HLINT ignore "Use map" #-}
 import System.Win32 (xBUTTON1)
 {-# HLINT ignore "Redundant lambda" #-}
 
@@ -70,3 +74,32 @@ uncurry f = \(x, y) -> f x y
 -- 2.c
 {- no es posible, ya que en haskell no hay manera (al menos de forma simple) de que la funcion sepa cuantos argumentos tiene de forma arbitraria.
 Esto se debe a que en haskell todas las funciones son, de un solo argumento (las que parecen tener varios en realidad devuelven una función que toma el siguiente argumento, y así sucesivamente) -}
+
+-- 3.a
+
+sumFoldr :: [Int] -> Int
+sumFoldr nums = foldr (+) 0 nums
+
+elemFoldr :: Eq a => a -> [a] -> Bool
+elemFoldr x = foldr (\y acc -> x == y || acc) False
+
+concatFoldr :: [a] -> [a] -> [a]
+concatFoldr s1 s2 = foldr (\x acc -> x : acc) s2 s1
+
+filterFoldr :: (a -> Bool) -> [a] -> [a]
+filterFoldr f = foldr (\x acc -> if f x then x : acc else acc) []
+
+mapFoldr :: (a -> b) -> [a] -> [b]
+mapFoldr f = foldr (\x acc -> f x : acc) []
+
+-- 3.b
+-- foldr1 f lista  (acc = last e de la lista)
+mejorSegun :: (a -> a -> Bool) -> [a] -> a
+mejorSegun f = foldr1 (\x acc -> if f x acc then x else acc)
+
+-- 3.c
+sumasParciales :: Num a => [a] -> [a]
+sumasParciales lista = foldl (\acc x -> acc ++ [x + sum (take (length acc) lista)]) [] lista
+
+
+res = sumasParciales [1, 4, -1, 0, 5] -- [1, 5, 4, 4, 9]
